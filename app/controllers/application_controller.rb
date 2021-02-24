088bf0b6be56f81fcfb13c0e82ebf6a0aafb57c6
Luke Gill <bagrad007@gmail.com>
2021-02-23 20:37:51 -0500

    commit tree b848b3246464a505efa6a9655ab71d4fe6f53f3e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
-require 'sinatra/base'
+require "sinatra/base"
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
+  get "/" do
+    erb :super_hero
+  end
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  post "/teams" do
+    params[:team][:members].each do |m|
+      Hero.new(m)
+    end
 
-
+    team_name = params[:team][:name]
+    motto_name = params[:team][:motto]
+    @team = Team.new(team_name, motto_name)
+    @heroes = Hero.all
+    # binding.pry
+    erb :team
+  end
 end
